66565a8ecdfb7d725e888c5292e3cdb307508e94
Artur <artur.azarau@gmail.com>
2019-06-13 19:01:58 +0300

    Initial Commit

---
+++ TestingAnimations/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestingAnimations
+//
+//  Created by Артур Азаров on 13/06/2019.
+//  Copyright © 2019 Артур Азаров. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
